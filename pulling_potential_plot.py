e6c51480238d08bef8348c9e8ef0645c08330a5f
MillerMC96 <pywang@ucdavis.edu>
2020-06-11 17:38:57 -0700

    beta version

--- pulling_potential_plot.py
+++ pulling_potential_plot.py
@@ -4,71 +4,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.animation import FuncAnimation
 import sys
 
-# read file from command line
-xvg_file = open(sys.argv[1], 'r')
-lines = xvg_file.readlines()
+def animation_frame(i):
+    # read file from command line
+    xvg_file = open(sys.argv[1], 'r')
+    lines = xvg_file.readlines()
+    
+    
+    # data arrays for analysis
+    
+    # pulling force at each time step
+    force = [] #kJ/mol/nm
+    
+    
+    # time step dt, constant
+    dt = 0.05 #ps
+    
+    # time
+    time = [] #ps
 
+    # window
+    N = int(0.1 / velocity / dt)
+    
+    # data entry
+    for line in lines:
+        line_entry = line.split()
+        # skip comments
+        first_charactor = line_entry[0]
+        if first_charactor[0] != '#' and first_charactor[0] != '@':
+            # read data
+            force.append(float(line_entry[1]))
+            time.append(float(line_entry[0]))
+            # moving mean
+            move_mean = np.convolve(force, np.ones((N,))/N, mode = 'same')
+            # energy calculation
+            energy_one_sum = move_mean * dt * velocity
+            energy = np.cumsum(energy_one_sum)
+    
+    # pull force
+    ax[0].clear()
+    ax[0].scatter(time, force, s = 2)
+    # plotting moving mean
+    ax[0].plot(time[N-1:-N], move_mean[N-1:-N], 'r', \
+               label = "moving average over " + str(N*dt) + " ps")
+    
+    ax[0].legend(loc = 'best')
+    # pull energy
+    ax[1].clear()
+    ax[1].scatter(time[N-1:-N], energy[N-1:-N], s = 2)
+    
 # customize title
 if len(sys.argv) > 3:
     fig_title = sys.argv[3]
 else:
     fig_title = ""
 
-# data arrays for analysis
-
-# pulling force at each time step
-force = [] #kJ/mol/nm
-
 # pulling velocity, assumed to be constant
 velocity = float(sys.argv[2]) #nm/ps
-
-# time step dt, constant
-dt = 0.05 #ps
-
-# time
-time = [] #ps
-
-# data entry
-for line in lines:
-    line_entry = line.split()
-    # skip comments
-    first_charactor = line_entry[0]
-    if first_charactor[0] != '#' and first_charactor[0] != '@':
-        # read data
-        force.append(float(line_entry[1]))
-        time.append(float(line_entry[0]))
-
-# window
-N = int(0.1 / velocity / dt)
-
-# moving mean
-move_mean = np.convolve(force, np.ones((N,))/N, mode = 'same')
-
-# moving standard deviation
-force_pd = pd.Series(force)
-move_std = force_pd.rolling(N).std()
-
-# energy calculation
-energy_one_sum = move_mean * dt * velocity
-energy = np.cumsum(energy_one_sum)
 
 # pull force and pulling energy
 fig, ax = plt.subplots(2, 1, sharex=True, figsize=(9.5,10))
 fig.suptitle("pulling force and energy along the trajectory " + fig_title)
 
-# pull force
-ax[0].scatter(time, force, s = 2)
-# plotting moving mean
-ax[0].plot(time[N-1:-N], move_mean[N-1:-N], 'r', \
-           label = "moving average over " + str(N*dt) + " ps")
+# proper labels
 ax[0].set(ylabel = "Force [kJ/mol/nm]")
-
-# pull energy
-ax[1].scatter(time[N-1:-N], energy[N-1:-N], s = 2)
 ax[1].set(ylabel = "Work [kJ/mol]")
 ax[1].set(xlabel = "time [ps]")
 
-ax[0].legend(loc = 'best')
+animation = FuncAnimation(fig, func=animation_frame, frames=np.arange(1,1000,1), \
+                          interval=1000, repeat=False)
 plt.show()
