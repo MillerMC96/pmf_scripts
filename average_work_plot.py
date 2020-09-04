bfe7f1446a40f2fd8ee41c6df764aa5d86826be2
MillerMC96 <pywang@ucdavis.edu>
2020-09-04 12:24:31 -0700

    revised for final draft

--- average_work_plot.py
+++ average_work_plot.py
@@ -36,7 +36,7 @@ def get_average_search_work(work_runs, force_runs):
 def get_average_search_work_from_file(search_work_file):
     search_work = open(search_work_file, 'r')
     lines = search_work.readlines()
-    
+
     search_arr = []
     for line in lines:
         if line[0] != '.':
@@ -74,32 +74,35 @@ def plot_average_work(time, N, runs, mean_work, jarzynski_work):
     fig, ax = plt.subplots(2, 1, sharex=True, figsize=(9.5,9))
     # bring plots closer
     plt.subplots_adjust(hspace=0.05)
-    fig.suptitle(fig_title, y=0.92, fontsize=20) 
-    
+    fig.suptitle(fig_title, y=0.92, fontsize=20)
+
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
 
     # mean work
     ax[0].plot(time[N-1:-N], mean_work[N-1:-N], \
-               label = "average work over " + str(runs) + " runs")
+               label = "avg. work, " + str(runs) + " runs")
     ax[0].set_ylabel("Work [kJ/mol]", fontsize=20)
     plt.setp(ax[0].get_yticklabels(), fontsize=20)
 
     # Jayzynsky mean work
     ax[1].plot(time[N-1:-N], jarzynski_work[N-1:-N], \
-               label = "Jarzynski average work over " + str(runs) + " runs")
+               label = "Jarzynski work, " + str(runs) + " runs")
     ax[1].set_ylabel("Work [kJ/mol]", fontsize=20)
     ax[1].set_xlabel("time [ps]", fontsize=20)
-    ax[1].legend(loc = 'lower right', fontsize=20)
+    ax[1].legend(loc = 'upper left', fontsize=20)
 
     return ax
 
 def plot_search_work(ax, mean_search_work, work_label, time):
-    spacing = 12
-    annotation = work_label + " = " + \
-                 f"{mean_search_work:.0f}" + " kJ/mol"
+    (bot,top) = ax[0].get_ylim()
+    spacing = 0.10*(top-bot)
+    ####annotation = work_label + " = " + \
+    ####             f"{mean_search_work:.0f}" + " kJ/mol"
+    hline_label = "avg. 1st passage work"
+    annotation =  f"{mean_search_work:.0f}" + " kJ/mol"
     ax[0].hlines(mean_search_work, xmin=0, xmax=time[-1], \
-                 label="passage work", color='k', linestyle='--')
+                 label=hline_label, color='k', linestyle='--')
     ax[0].text(time[-1], mean_search_work - spacing, annotation, ha="right", \
                size=20)
 
@@ -109,10 +112,10 @@ if __name__ == "__main__":
 
     # pulling rate
     velocity = float(sys.argv[2])
-    
+
     # option to plot search work
     enable_search_work = bool(int(sys.argv[3]))
-    
+
     if enable_search_work:
         # file that contains all search work
         search_work_file = sys.argv[4]
@@ -123,11 +126,11 @@ if __name__ == "__main__":
         # total search work
         average_search_work = get_average_search_work_from_file(search_work_file)
         # work for tail
-        average_search_work_tail = get_average_search_work_from_file(tail_work_file)
+        ####average_search_work_tail = get_average_search_work_from_file(tail_work_file)
         # array of two search work
         search_work = []
         search_work.append(average_search_work)
-        search_work.append(average_search_work_tail)
+        ####search_work.append(average_search_work_tail)
 
         # customize title
         if len(sys.argv) > 6:
@@ -139,35 +142,35 @@ if __name__ == "__main__":
             fig_title = sys.argv[4]
         else:
             fig_title = ""
-    
+
     directory = os.fsencode(xvg_folder)
 
     # data structure that contains all runs
     work_runs = []
-    
+
     # data structure that contains all forces
     force_runs = []
-    
+
     for file in os.scandir(directory):
         filename = os.fsdecode(file)
          # only reads xvgs
-        if filename.endswith(".xvg"): 
+        if filename.endswith(".xvg"):
             one_time, N, one_work, force = get_one_work(filename, velocity)
             work_runs.append(one_work)
             force_runs.append(force)
-    
+
     num_of_runs = len(work_runs)
     # standard mean work
     mean_work = np.mean(work_runs, axis=0)
     jarzynski_work = get_jarzynski_work(work_runs)
     ax = plot_average_work(one_time, N, num_of_runs, mean_work, \
                            jarzynski_work)
-    
+
     # option to not plot search work
     if enable_search_work:
         plot_search_work(ax, search_work[0], "headgroup", one_time)
-        ax[0].legend(loc = 'lower right', fontsize=16)
-        plot_search_work(ax, search_work[1], "tail", one_time)
+        ax[0].legend(loc = 'lower right', fontsize=20)
+        ####plot_search_work(ax, search_work[1], "tail", one_time)
 
     save_figure = True
     # option to save figure
@@ -176,4 +179,3 @@ if __name__ == "__main__":
         plt.savefig(fig_title+".pdf")
     else:
         plt.show()
-
